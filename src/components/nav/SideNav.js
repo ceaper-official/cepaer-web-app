c9361924c01a3b9b53253dfc79a91f1b7efa85e5
Kerry <info@svillon.com>
2020-08-01 16:47:05 +0900

    Change: SideNav.js モバイル時の挙動

--- src/components/nav/SideNav.js
+++ src/components/nav/SideNav.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from 'react';
 import Link from "next/link";
 
+import Down from '@icons/ui/down'
 import s from "./Nav.module.scss";
 
 function  SideNav ({children}) {
+
+  const [open, setContentState] = useState(false);
+
   return (
+    <>
+    <div className={open ? s.open : ''}>
+      <span
+        className={s.side__nav__icon}
+        onClick={() => setContentState(!open)}>
+          <Down/>
+      </span>
+    </div>
+    <div className={open ? s.open : ''}>
+      <div className={s.side__mobile__nav}>
+        {children}
+      </div>
+    </div>
     <div className={s.side__nav}>
       {children}
     </div>
+    </>
   );
 }
 
