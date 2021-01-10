95c79cb386729b4270a49b278b5f723525dc636e
Kerry <hello.kerrytokyo@gmail.com>
2021-01-10 11:40:32 +0900

    fix: /forgot-password-inputの削除

--- src/pages/forgot-password.js
+++ src/pages/forgot-password.js
@@ -1,20 +1,9 @@
-import Link from "next/link";
 import React from "react";
 
 import BaseLayout from "@components/layout/BaseLayout";
 import Hero from "@components/hero/Hero";
-import Content from "@components/content/Content";
 import CardForm from "@components/form/CardForm";
-import Input from "@components/form/Input";
-import SectionItem from "@components/form/SectionItem";
-import LinkItem from "@components/form/LinkItem";
-import NoteItem from "@components/form/NoteItem";
 import Button from "@components/button/Button";
-import SocialButton from "@components/button/SocialButton";
-
-import Cross from "@icons/ui/cross.js";
-import Mail from "@icons/ui/mail.js";
-import Password from "@icons/ui/password.js";
 
 class ForgotPS extends React.Component {
   state = { Component: null };
@@ -29,16 +18,12 @@ class ForgotPS extends React.Component {
         <Hero>
           <CardForm title="パスワードの再設定">
             パスワードのリセット手順を送信いたしますので、対象アカウントで登録されたEメールをご入力ください。届いたメールの内容に沿って、パスワードの変更手続きを進めてください。
-            <Input
-              placeholder="Eメール"
-              icon={<Mail/>}
-            />
             <Button onClick={this.select}>
               パスワードを再設定
             </Button>
           </CardForm>
-      </Hero>
-    </BaseLayout>
+        </Hero>
+      </BaseLayout>
     );
   }
 }
