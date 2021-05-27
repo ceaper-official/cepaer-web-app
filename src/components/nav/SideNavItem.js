20b3e695bbc1bb575a951d3749ad8fdd4861c4f0
yuji oda <ug0416.33402@gmail.com>
2021-05-27 17:22:11 +0900

    provider毎のEditNavの表示変更

--- src/components/nav/SideNavItem.js
+++ src/components/nav/SideNavItem.js
@@ -4,10 +4,10 @@ import { useRouter } from "next/router";
 
 import s from "./Nav.module.scss";
 
-function SideNavItem ({ to, title}) {
+function SideNavItem ({ to, title, style}) {
   const router = useRouter();
     return (
-      <div className={s.side__nav__item}>
+      <div className={s.side__nav__item} style={style}>
         <Link href={to}  activeClassName={s.active}>
           <a className={s.side__nav__link}>
             {title}
--- src/pages/edit/EditNav.js
+++ src/pages/edit/EditNav.js
@@ -1,10 +1,31 @@
 import React from "react";
 
+import { firebase, getCurrentUser } from "@src/firebase";
+
 import SideNav from "@components/nav/SideNav";
 import SideNavItem from "@components/nav/SideNavItem";
 
 export class Nav extends React.Component {
+  constructor(props) {
+    // Stateの定義
+    super(props);
+    this.state = {
+      providerID: "",
+    };
+  }
+
+  componentDidMount() {
+    const user = firebase.auth().currentUser;
+    if(user) {
+      user.providerData.forEach((profile) => {
+      this.setState({providerID: profile.providerId});
+      });
+    }
+  };
+
   render() {
+    const loginProvider = this.state.providerID;
+    console.log(loginProvider);
     return (
       <SideNav>
         <SideNavItem
@@ -17,16 +38,17 @@ export class Nav extends React.Component {
         />
         <SideNavItem
           title="パスワードの変更"
-           to="/edit/password"
-          />
-          <SideNavItem
+          to="/edit/password"
+          style={{ display: loginProvider !== "password" ? 'none' : '' }}
+        />
+        <SideNavItem
            title="ソーシャルメディア"
             to="/edit/social"
-          />
-          <SideNavItem
+        />
+        <SideNavItem
            title="アカウントの削除"
             to="/edit/delete"
-          />
+        />
         </SideNav>
     );
   }
